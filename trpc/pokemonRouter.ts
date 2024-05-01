fb002b59693f2d032d45644dc2040e13540cf40c
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-05-01 16:25:35 +0530

    ..

--- trpc/pokemonRouter.ts
+++ trpc/pokemonRouter.ts
@@ -1,37 +1,32 @@
-// import { createTRPCRouter } from '@trpc/server';
-import { createTRPCRouter } from '@trpc/server/router';
+import { router, publicProcedure } from './trpc';
 import { z } from 'zod';
 import { prisma } from '../prisma';
 
-const pokemonRouter = createTRPCRouter()
- .query('getPokemon', {
-    input: z.string(),
-    async resolve({ input }) {
+const pokemonRouter = router({
+  getPokemon: publicProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
       const pokemon = await prisma.pokemon.findUnique({
         where: { name: input },
       });
       return pokemon;
-    },
-  })
- .query('getPokemonArray', {
-    input: z.array(z.string()),
-    async resolve({ input }) {
+    }),
+  getPokemonArray: publicProcedure
+    .input(z.array(z.string()))
+    .query(async ({ input }) => {
       const pokemonArray = await prisma.pokemon.findMany({
         where: { name: { in: input } },
       });
       return pokemonArray;
-    },
-  })
- .query('getFilteredPokemonArray', {
-    input: z.object({ type: z.string().optional() }),
-    async resolve({ input }) {
+    }),
+  getFilteredPokemonArray: publicProcedure
+    .input(z.object({ type: z.string().optional() }))
+    .query(async ({ input }) => {
       const pokemonArray = await prisma.pokemon.findMany({
-        where: input.type
-         ? { types: { has: input.type } }
-          : {},
+        where: input.type ? { types: { has: input.type } } : {},
       });
       return pokemonArray;
-    },
-  });
+    }),
+});
 
 export default pokemonRouter;
