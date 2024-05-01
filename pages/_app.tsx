cbc0f8dd562debb01f2efe084cd5ae841adf99b4
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-05-01 16:14:07 +0530

    added

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { trpc } from '../trpc';
+import PokemonForm from '../components/PokemonForm';
+import PokemonArrayForm from '../components/PokemonArrayForm';
+import FilterablePokedexTable from '../components';
