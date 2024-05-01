c9ee928ab2af0f3d8bf5c0304d71b6371a768d88
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-05-01 16:06:24 +0530

    Create FilterablePokedexTable.tsx

---
+++ components/FilterablePokedexTable.tsx
@@ -0,0 +1,35 @@
+// components/FilterablePokedexTable.tsx
+import * as React from 'react';
+import PokemonTypeSelection from '../components/PokemonTypeSelection';
+import PokedexTable from '../components/PokedexTable';
+
+interface FilterablePokedexTableProps {
+  pokemonArray: {
+    id: number;
+    name: string;
+    types: string[];
+    sprite: string;
+  }[];
+  selectedType: string | undefined;
+}
+
+const FilterablePokedexTable: React.FC<FilterablePokedexTableProps> = ({
+  pokemonArray,
+  selectedType,
+}) => {
+  const filteredPokemonArray = pokemonArray.filter((pokemon) =>
+    selectedType ? pokemon.types.includes(selectedType) : true
+  );
+
+  return (
+    <div>
+      <PokemonTypeSelection
+        selectedType={selectedType}
+        selectType={(type) => console.log(type)}
+      />
+      <PokedexTable pokemonArray={filteredPokemonArray} />
+    </div>
+  );
+};
+
+export default FilterablePokedexTable;
