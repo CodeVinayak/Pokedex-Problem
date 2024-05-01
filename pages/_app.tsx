aa28d54fca1d03f2e959d4758f34f127c00a572e
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-05-01 16:48:32 +0530

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,4 +2,14 @@ import * as React from 'react';
 import { trpc } from '../trpc';
 import PokemonForm from '../components/PokemonForm';
 import PokemonArrayForm from '../components/PokemonArrayForm';
-import FilterablePokedexTable from '../components';
+import { FilterablePokedexTable } from '../components/FilterablePokedexTable';
+
+import withTRPC from '../trpc';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <withTRPC>
+      <Component {...pageProps} />
+    </withTRPC>
+  );
+}
--- trpc/trpc.ts
+++ trpc/trpc.ts
@@ -1,6 +1,16 @@
 import { initTRPC } from '@trpc/server';
+import { TRPCContext } from '@trpc/react';
 
 const t = initTRPC.create();
 
-export const router = t.router;
-export const publicProcedure = t.procedure;
+export const trpc = t.createTRPCContext();
+
+export function withTRPC(App: React.ComponentType) {
+  return function TRPCApp(props: any) {
+    return (
+      <TRPCContext.Provider value={trpc}>
+        <App {...props} />
+      </TRPCContext.Provider>
+    );
+  };
+}
